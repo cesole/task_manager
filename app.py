7be9291406353d160b8ee49429a01937bafe0364
Ubuntu <ubuntu@ip-172-31-89-3.ec2.internal>
2019-08-09 14:07:26 +0000

    add task btn

--- app.py
+++ app.py
@@ -13,14 +13,24 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", 
-                           tasks=mongo.db.tasks.find())
+    _tasks = mongo.db.tasks.find()
+    task_list = [task for task in _tasks]
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html',
-                           categories=mongo.db.categories.find())
+    _categories = mongo.db.categories.find()
+    category_list = [category for category in _categories]
+    return render_template('addtask.html', categories=mongo.db.categories.find())
+    
+    
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+    
 
 
 if __name__ == '__main__':
